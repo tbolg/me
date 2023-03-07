eb8858301f9454649cf061a026904c425fb78690
Tomas Bolger <tbolger.business@gmail.com>
2023-03-07 22:26:17 +1100

    Revert changes (#11)
    * Revert "Add download button (#10)"
    
    This reverts commit 30d8e4fc13870169cdbd0d0dce4f11c8d126ecc4.
    
    * Keep some changes

--- src/App/views/Homepage/components/Header.tsx
+++ src/App/views/Homepage/components/Header.tsx
@@ -11,7 +11,6 @@ import {
   IconMail,
   useResponsiveValue,
   ButtonIcon,
-  IconDownload,
 } from 'braid-design-system';
 import React from 'react';
 
@@ -38,20 +37,9 @@ export const Header = () => {
               <ButtonIcon
                 size="large"
                 bleed
-                icon={<IconDownload />}
-                id="download-resume"
-                label="Download resume"
-                onClick={() =>
-                  window.open('/resume_tomas_bolger.pdf', '_blank')
-                }
-              />
-
-              <ButtonIcon
-                size="large"
-                bleed
                 icon={<IconSocialGitHub />}
-                id="github-link"
-                label="GitHub"
+                id={'github-link'}
+                label={'GitHub'}
                 onClick={() =>
                   window.open('https://github.com/tbolg', '_blank')
                 }
@@ -60,8 +48,8 @@ export const Header = () => {
                 size="large"
                 bleed
                 icon={<IconSocialLinkedIn />}
-                id="linkedin-link"
-                label="LinkedIn"
+                id={'linkedin-link'}
+                label={'LinkedIn'}
                 onClick={() =>
                   window.open(
                     'https://www.linkedin.com/in/tomas-bolger/',
